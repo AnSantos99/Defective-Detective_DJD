5ddc08e69ecd1a89926569a5fb249d2c7a271ddc
BananaCat <joana.f.silva.99@gmail.com>
2019-11-15 17:10:33 +0000

    Added reticle

---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+
+    private RectTransform _reticle;
+
+
+    private void Start()
+    {
+        _reticle = GetComponent<RectTransform>();
+    }
+}
