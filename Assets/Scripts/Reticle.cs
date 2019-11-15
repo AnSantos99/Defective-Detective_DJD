0a14684f0b10998c2cb0bb804368f7141b90f486
BananaCat <joana.f.silva.99@gmail.com>
2019-11-15 17:32:22 +0000

    Changed Reticle

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -8,9 +8,17 @@ public class Reticle : MonoBehaviour
 
     private RectTransform _reticle;
 
+    [Range(50f, 250)]
+    public float size;
+
 
     private void Start()
     {
         _reticle = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        _reticle.sizeDelta = new Vector2(size, size);
     }
 }
