bbee2a8a3431976b31accb83b2b89b29a0eb490c
BananaCat <joana.f.silva.99@gmail.com>
2019-12-17 17:08:49 +0000

    Added Item and Lock Puzzle Script

---
+++ Assets/CodeLock.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeLock : MonoBehaviour
+{
+    int codeLength;
+    int placeInCode;
+
+    public string code = "";
+    public string attemptedCode;
+
+    public Transform toOpen;
+
+    private void Start()
+    {
+        codeLength = code.Length;
+    }
+
+    void CheckCode()
+    {
+        if(attemptedCode == code)
+        {
+            StartCoroutine(Open());
+        }
+        else
+        {
+            Debug.Log("WrongCode");
+        }
+    }
+
+    IEnumerator Open()  //'animation' of open & close
+    {
+        toOpen.Rotate(new Vector3(90, 0, 0), Space.World);
+
+        yield return new WaitForSeconds(4);
+
+        toOpen.Rotate(new Vector3(-90, 0, 0), Space.World);
+    }
+
+    public void SetValue(string value)
+    {
+        placeInCode++;
+
+        if(placeInCode <= codeLength)
+        {
+            attemptedCode += value;
+        }
+
+        if(placeInCode == codeLength)
+        {
+            CheckCode();
+
+            attemptedCode = "";
+            placeInCode = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    CodeLock codeLock;
+    private GameObject cursor;
+
+    int reachRange = 100;
+    
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CheckHitObj();
+        }
+    }
+
+    void CheckHitObj()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, reachRange))
+        {
+            codeLock = hit.transform.gameObject.GetComponentInParent<CodeLock>();//Witch codelock is beeing pressed
+
+            if (codeLock != null)
+            {
+                string value = hit.transform.name;
+                codeLock.SetValue(value);
+            }
+        }
+    }
+}
