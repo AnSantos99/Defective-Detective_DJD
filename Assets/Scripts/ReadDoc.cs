19791e355c9db3d11ae4e3f6b16207f5c9fb86f2
nanilev <dianuski99@gmail.com>
2020-01-09 23:17:02 +0000

    add new book mechaninc (initial stages)

---
+++ Assets/Scripts/ReadDoc.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadDoc : MonoBehaviour
+{
+
+    private GameObject Interactable;
+
+    void Touch()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 10f))
+        {
+
+            if (hit.collider.gameObject.tag == "Touchable") // Did it hit the paper?
+            {
+                // Show the paper
+                hit.collider.gameObject.SendMessage("OnTouch");
+                Interactable = hit.collider.gameObject;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // If the player clicks with the left mouse button
+        {
+            if (Interactable == null)
+            {
+                Touch(); // then execute Touch()
+            }
+            else
+            {
+                Interactable.SendMessage("UnTouch");
+                Interactable = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ReadObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadObjects : MonoBehaviour
+{
+    //public GameObject displayImage;
+    private GameObject view;
+    private ViewController viewScript;
+
+
+
+    void Start()
+    {
+        view = GameObject.FindGameObjectWithTag("viewManager");
+        viewScript = view.GetComponent<ViewController>();
+    }
+
+    void OnTouch()
+    {       
+        viewScript.ToggleInputPanel(false);
+    }
+
+    void UnTouch()
+    {
+        viewScript.ToggleInputPanel(true);
+    }
+}
