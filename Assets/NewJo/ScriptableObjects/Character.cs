6d0889ade75f5a0b26f6ebbb871c219dd90c83fa
BananaCat <joana.f.silva.99@gmail.com>
2020-01-21 17:20:23 +0000

    Added dialogue scripts

---
+++ Assets/NewJo/ScriptableObjects/Character.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Name Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+    public string fullName;
+    public Sprite portrait;
+}
--- Assets/Scripts/FinalScripts/DialogueSystem.cs
+++ Assets/Scripts/FinalScripts/DialogueSystem.cs
@@ -43,6 +43,7 @@ public class DialogueSystem : MonoBehaviour
         if (dialogueActive == true)
         {
             dialogueGUI.SetActive(false);
+            
         }
     }
 
