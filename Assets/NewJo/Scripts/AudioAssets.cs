eec969150d77bd3313090d7aa56beb94877046c1
BananaCat <joana.f.silva.99@gmail.com>
2020-01-23 20:47:40 +0000

    Audio

---
+++ Assets/NewJo/Scripts/AudioAssets.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioAssets
+{
+    public string name;
+
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
---
+++ Assets/NewJo/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class SoundManager: MonoBehaviour
+{
+    public AudioAssets[] sounds;
+
+    private void Awake()
+    {
+        foreach (AudioAssets s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        AudioAssets s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
